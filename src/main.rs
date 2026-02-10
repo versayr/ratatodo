d93a174e38a00ad570bcec5119c246d4bada2908
Bruce Manning <themoose3@gmail.com>
2026-02-10 14:19:25 -0500

    Create task list layout

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use std::io;
+use std::{io, vec};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
-    style::Stylize,
+    style::{
+        palette::tailwind::{BLUE, GREEN, SLATE},
+        Color, Modifier, Style, Stylize,
+    },
     symbols::border,
-    text::{Line},
-    widgets::{Block, Borders, Paragraph, Widget},
+    text::Line,
+    widgets::{
+        Block, Borders, HighlightSpacing, List, ListItem, ListState, Paragraph, StatefulWidget,
+        Widget,
+    },
     DefaultTerminal, Frame,
 };
+
+const TODO_HEADER_STYLE: Style = Style::new().fg(SLATE.c100).bg(BLUE.c800);
+const NORMAL_ROW_BG: Color = SLATE.c950;
+const ALT_ROW_BG_COLOR: Color = SLATE.c900;
+const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+const TEXT_FG_COLOR: Color = SLATE.c200;
+const COMPLETED_TEXT_FG_COLOR: Color = GREEN.c500;
+
+const fn alternate_colors(i: usize) -> Color {
+    if i.is_multiple_of(2) {
+        NORMAL_ROW_BG
+    } else {
+        ALT_ROW_BG_COLOR
+    }
+}
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
 
-    let mut app = App { exit: false };
+    let mut app = App::default();
 
     let app_result = app.run(&mut terminal);
 
@@ -24,6 +46,26 @@ fn main() -> io::Result<()> {
 
 pub struct App {
     exit: bool,
+    list: TodoList,
+}
+
+struct TodoList {
+    items: Vec<Task>,
+    state: ListState,
+}
+
+#[derive(Debug)]
+struct Task {
+    todo: String,
+    info: String,
+    status: Status,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Status {
+    Upcoming,
+    Active,
+    Completed,
 }
 
 impl App {
@@ -38,7 +80,7 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
+    fn draw(&mut self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
     }
 
@@ -69,18 +111,51 @@ impl App {
     fn exit(&mut self) {
         self.exit = true;
     }
+
+    fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .title(Line::raw("Task List").centered())
+            .borders(Borders::TOP)
+            .border_set(border::EMPTY);
+
+        // Iterate through all elements in the `items` and stylize them.
+        let items: Vec<ListItem> = self
+            .list
+            .items
+            .iter()
+            .enumerate()
+            .map(|(i, todo_item)| {
+                let color = alternate_colors(i);
+                ListItem::from(todo_item).bg(color)
+            })
+            .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        let list = List::new(items)
+            .block(block)
+            .highlight_style(SELECTED_STYLE)
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+        // same method name `render`.
+        StatefulWidget::render(list, area, buf, &mut self.list.state);
+    }
 }
 
-impl Widget for &App {
+impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(" Ratatodo ".bold());
         let instructions = Line::from(vec![
-            " [N]ew Task ".into(),
-            "<N>".blue().bold(),
-            " [M]ark Completed ".into(),
-            "<M>".blue().bold(),
-            " [Q]uit ".into(),
-            "<Q> ".blue().bold(),
+            " [".into(),
+            "N".blue().bold(),
+            "]ew Task".into(),
+            " [".into(),
+            "M".blue().bold(),
+            "]ark Completed".into(),
+            " [".into(),
+            "Q".blue().bold(),
+            "]uit ".into(),
         ]);
 
         let block = Block::bordered()
@@ -90,28 +165,66 @@ impl Widget for &App {
 
         let layout = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Percentage(30),
-                Constraint::Percentage(40),
-                Constraint::Percentage(30),
-            ])
+            .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
             .split(Block::inner(&block, area));
 
-        Paragraph::new("left")
-            .centered()
-            .block(Block::new().borders(Borders::ALL))
-            .render(layout[0], buf);
-
-        Paragraph::new("center")
+        Paragraph::new("details")
             .centered()
             .block(Block::new().borders(Borders::ALL))
             .render(layout[1], buf);
 
-        Paragraph::new("right")
-            .centered()
-            .block(Block::new().borders(Borders::ALL))
-            .render(layout[2], buf);
-
         block.render(area, buf);
+        self.render_list(layout[0], buf);
+    }
+}
+
+impl Task {
+    fn new(status: Status, todo: &str, info: &str) -> Self {
+        Self {
+            status,
+            todo: todo.to_string(),
+            info: info.to_string(),
+        }
+    }
+}
+
+impl From<&Task> for ListItem<'_> {
+    fn from(value: &Task) -> Self {
+        let line = match value.status {
+            Status::Upcoming => Line::styled(format!(" ☐ {}", value.todo), TEXT_FG_COLOR),
+            Status::Active => Line::styled(format!(" ☐ {}", value.todo), TEXT_FG_COLOR),
+            Status::Completed => {
+                Line::styled(format!(" ✓ {}", value.todo), COMPLETED_TEXT_FG_COLOR)
+            }
+        };
+        ListItem::new(line)
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            exit: false,
+            list: TodoList::from_iter([
+                (
+                    Status::Upcoming,
+                    "Write down some tasks",
+                    "Use this todo list tui application.",
+                ),
+                (Status::Active, "Relax", "Well done."),
+                (Status::Completed, "Get list items rendering", "Well done."),
+            ]),
+        }
+    }
+}
+
+impl FromIterator<(Status, &'static str, &'static str)> for TodoList {
+    fn from_iter<I: IntoIterator<Item = (Status, &'static str, &'static str)>>(iter: I) -> Self {
+        let items = iter
+            .into_iter()
+            .map(|(status, todo, info)| Task::new(status, todo, info))
+            .collect();
+        let state = ListState::default();
+        Self { items, state }
     }
 }
